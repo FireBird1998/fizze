e692b793e33d81a8bcd8b62e35dad86692ec9e45
ankit-thesys <i-ankit@thesys.dev>
2024-09-20 19:23:46 +0530

    hero section 3d fied

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,16 +1,23 @@
 'use client'
+
 import { asText, Content } from '@prismicio/client'
-import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
+import { PrismicNextImage } from '@prismicio/next'
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { View } from '@react-three/drei'
 
 import { Bounded } from '@/components/Bounded'
 import Button from '@/components/Button'
 import { TextSplitter } from '@/components/TextSplitter'
+import Scene from './Scene'
+import { Bubbles } from './Bubbles'
+import { useStore } from '@/hooks/useStore'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
 gsap.registerPlugin(useGSAP, ScrollTrigger)
+
 /**
  * Props for `Hero`.
  */
@@ -20,77 +27,79 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  useGSAP(() => {
-    const introTL = gsap.timeline()
+  const ready = useStore((state) => state.ready)
+  const isDesktop = useMediaQuery('(min-width: 768px)', true)
 
-    introTL
-      .set('.hero', { opacity: 1 })
-      .from('.hero-header-word', {
-        scale: 3,
-        opacity: 0,
-        ease: 'power4.in',
-        delay: 0.3,
-        stagger: 1,
-      })
-      .from(
-        '.hero-subheader',
-        {
+  useGSAP(
+    () => {
+      if (!ready && isDesktop) return
+
+      const introTl = gsap.timeline()
+
+      introTl
+        .set('.hero', { opacity: 1 })
+        .from('.hero-header-word', {
+          scale: 3,
           opacity: 0,
-          y: 30,
-        },
-        '+=.8'
-      )
-      .from('.hero-body', {
-        opacity: 0,
-        y: 10,
-      })
-      .from(
-        '.hero-button',
-        {
+          ease: 'power4.in',
+          delay: 0.3,
+          stagger: 1,
+        })
+        .from(
+          '.hero-subheading',
+          {
+            opacity: 0,
+            y: 30,
+          },
+          '+=.8'
+        )
+        .from('.hero-body', {
           opacity: 0,
           y: 10,
-        },
-        '+=.6'
-      )
+        })
+        .from('.hero-button', {
+          opacity: 0,
+          y: 10,
+          duration: 0.6,
+        })
 
-    const scrollTL = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.hero',
-        start: 'top top',
-        end: 'bottom bottom',
-        scrub: 1.5,
-        markers: true,
-      },
-    })
+      const scrollTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.hero',
+          start: 'top top',
+          end: 'bottom bottom',
+          scrub: 1.5,
+        },
+      })
 
-    scrollTL
-      .fromTo(
-        'body',
-        {
-          backgroundColor: '#FDE047',
-        },
-        {
-          backgroundColor: '#D9F99D',
-          overwrite: 'auto',
-        },
-        1
-      )
-      .from('.text-side-heading .split-char', {
-        scale: 1.3,
-        y: 40,
-        rotate: -25,
-        opacity: 0,
-        stagger: 0.1,
-        ease: 'back.out(3)',
-        duration: 0.5,
-      })
-      .from('.text-side-body', {
-        y: 40,
-        opacity: 0,
-        duration: 0.5,
-        ease: 'back.out(4)',
-      })
-  })
+      scrollTl
+        .fromTo(
+          'body',
+          {
+            backgroundColor: '#FDE047',
+          },
+          {
+            backgroundColor: '#D9F99D',
+            overwrite: 'auto',
+          },
+          1
+        )
+        .from('.text-side-heading .split-char', {
+          scale: 1.3,
+          y: 40,
+          rotate: -25,
+          opacity: 0,
+          stagger: 0.1,
+          ease: 'back.out(3)',
+          duration: 0.5,
+        })
+        .from('.text-side-body', {
+          y: 20,
+          opacity: 0,
+        })
+    },
+    { dependencies: [ready, isDesktop] }
+  )
 
   return (
     <Bounded
@@ -98,6 +107,13 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
+      {isDesktop && (
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles count={300} speed={2} repeat={true} />
+        </View>
+      )}
+
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
@@ -108,10 +124,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                 className="hero-header-word"
               />
             </h1>
-            <div className="hero-subheader mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
+            <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
               <PrismicRichText field={slice.primary.subheading} />
             </div>
-            <div className="hero-body mt-5 text-2xl font-normal text-sky-950">
+            <div className="hero-body text-2xl font-normal text-sky-950">
               <PrismicRichText field={slice.primary.body} />
             </div>
             <Button
